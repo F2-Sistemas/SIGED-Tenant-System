5d4f9a3392174d5c22206d48275f9f3a6e39790d
Tiago França <tiago@tiagofranca.com>
2023-08-18 07:03:56 -0300

    updated dependencies
    On branch feature/web-accordion-orcamentos
    Changes to be committed:
    	modified:   composer.lock
    	modified:   package.json
    	modified:   pnpm-lock.yaml
    	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import flowbite from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        './node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+        './node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
@@ -18,5 +21,38 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        flowbite,
+        forms,
+    ],
 };
+
+// --------------------
+// // wrorks from here
+
+// const DefaultTheme = require('tailwindcss/defaultTheme');
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: [
+//         './node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+//         './node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+//         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+//         './storage/framework/views/*.php',
+//         './resources/views/**/*.blade.php',
+//         './resources/js/**/*.vue',
+//     ],
+
+//     theme: {
+//         extend: {
+//             fontFamily: {
+//                 sans: ['Figtree', ...DefaultTheme.fontFamily.sans],
+//             },
+//         },
+//     },
+
+//     plugins: [
+//         require('flowbite/plugin'),
+//         require('@tailwindcss/forms'),
+//     ],
+// }
